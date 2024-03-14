41bb2acb96b50ca18044e8dba870279a54444313
casalaspro <casalaspro.alessandro@gmail.com>
2024-03-14 15:29:55 +0100

    REFACTORY - createGrid in a function

--- js/app.js
+++ js/app.js
@@ -6,40 +6,52 @@ const containerElement = document.querySelector(".grid");
 
 // attribuire eventListener al bottone play che:
 buttonElement.addEventListener("click", function () {
+  
   const selectElement = document.getElementById("difficulty");
   const selectValue = parseInt(selectElement.value);
-  console.log(selectValue);
 
-  // reset grid
-  containerElement.innerHTML = '';
+  
   
   let numberCells;
-  let difficultyClass;
+  let difficultyClasses;
 
+  
   if (selectValue === 100) {
     numberCells = selectValue;
-    difficultyClass = "difficulty-1"
+    difficultyClasses = "difficulty-1"
   } else if (selectValue === 81) {
     numberCells = selectValue;
-    difficultyClass = "difficulty-2"
+    difficultyClasses = "difficulty-2"
   } else if (selectValue === 49) {
     numberCells = selectValue;
-    difficultyClass = "difficulty-3"
+    difficultyClasses = "difficulty-3"
   } else {
     console.log("Choose a difficulty before to star the game.")
   }
   
-  for (let i = 1; i <= selectValue; i++) {
-    let number = i;
-    const divElement = document.createElement("div");
-    divElement.classList.add("cell", difficultyClass);
-    const textElement = document.createTextNode(number);
-    divElement.appendChild(textElement);
-    divElement.addEventListener("click", function () {
-      this.classList.toggle('cyan');
-      console.log(number);
-    });
-    containerElement.appendChild(divElement);
-    console.log(divElement);
+
+  createGrid(numberCells, difficultyClasses);
+
+  
+
+  function createGrid(cells, difficulty){
+    // reset grid
+    containerElement.innerHTML = '';
+
+    for (let i = 1; i <= cells; i++) {
+      let number = i;
+      const divElement = document.createElement("div");
+      divElement.classList.add("cell", difficulty);
+      const textElement = document.createTextNode(number);
+      divElement.appendChild(textElement);
+      divElement.addEventListener("click", function () {
+        this.classList.toggle('cyan');
+      });
+      containerElement.appendChild(divElement);
+    }
   }
+  
 })
+
+
+
